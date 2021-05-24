0e505e29b5d45903d342848d4887a59be2a282a3
ivaste <stefano.ivancich@studenti.unipd.it>
2021-05-24 21:36:10 +0200

    KMP

--- kmp.py
+++ kmp.py
@@ -38,10 +38,10 @@ def KMP(string, substring):
                 return True
     return False
 
-
-
-reference_file="rfc7552.txt"
-file_to_check=""
+######################################################
+window_size=4
+reference_file="reference_files/rfc7252.txt"
+file_to_check="files_to_check/IoT_CoAP (1).txt"
 
 #Read reference
 f = open(reference_file, "r")
@@ -49,6 +49,57 @@ text=f.read()
 f.close()
 
 #Read pattern
-f = open(file_to_check, "r")
+f = open(file_to_check, "r",encoding='utf-8')
 string_to_check=f.read()
 f.close()
+string_to_check=string_to_check.replace("\n", " ")
+string_to_check=string_to_check.split(" ")
+
+print("Reference Text:",str(len(text.split(" "))),"words")
+print("File to check:",len(string_to_check),"words")
+print("Window size:",window_size,"words")
+
+######################################################
+ans=[False]*len(string_to_check)
+for i in range(len(string_to_check)-window_size):
+    print("Matching: "+str(100*i//len(string_to_check))+"%",end="\r", flush=True)
+
+    pattern=" ".join(string_to_check[i:i+window_size])
+    if KMP(text,pattern):
+        for j in range(i,i+window_size):
+            ans[j]=True
+
+######################################################
+print("Saving...")
+
+solution=[]
+for i in range(len(ans)):
+    if ans[i]:
+        solution.append('<span style="color: red">')
+        solution.append(string_to_check[i])
+        solution.append("</span>")
+    else:
+        solution.append(string_to_check[i])
+solution=" ".join(solution)
+
+import time
+named_tuple = time.localtime() # get struct_time
+time_string = time.strftime("%Y-%m-%d_%H-%M-%S", named_tuple)
+
+f = open("solution_"+time_string+".html", "wt",encoding='utf-8')
+f.write(solution)
+f.close()
+print("Saved")
+
+
+
+
+"""
+TODO:
+    - load pdf and convert it to txt
+    - select files from command line
+    - multiple references files
+    - Better pattern matching algorithm
+
+
+"""
